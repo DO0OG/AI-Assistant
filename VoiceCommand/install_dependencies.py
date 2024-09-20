3a4b7a8d5048dde2c1329b05af9ee139331f3184
MAD_DOGGO <tkatlrdldi@gmail.com>
2024-09-20 16:53:41 +0900

    feat: Script Modified

--- VoiceCommand/install_dependencies.py
+++ VoiceCommand/install_dependencies.py
@@ -7,10 +7,7 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
-def main():
-    print("필요한 패키지를 설치합니다...")
-
-    # requirements.txt 파일 생성
+def create_requirements_file():
     requirements = [
         "pycaw",
         "keyboard",
@@ -36,13 +33,19 @@ def main():
         "urllib3",
         "datetime",
         "python-dotenv",
-        "pyinstaller"
+        "pyinstaller",
     ]
 
     with open("requirements.txt", "w") as f:
         for req in requirements:
             f.write(f"{req}\n")
 
+
+def main():
+    print("필요한 패키지를 설치합니다...")
+
+    # requirements.txt 파일 생성
+    create_requirements_file()
     print("requirements.txt 파일을 생성했습니다.")
 
     # requirements.txt 파일에서 패키지 설치
@@ -54,16 +57,8 @@ def main():
         print("requirements.txt 파일을 찾을 수 없거나 설치 중 오류가 발생했습니다.")
         return
 
-    print("MeloTTS를 설치합니다...")
-
-    # MeloTTS 설치
-    try:
-        install("git+https://github.com/myshell-ai/MeloTTS.git")
-    except subprocess.CalledProcessError:
-        print("MeloTTS 설치 중 오류가 발생했습니다.")
-        return
-
     print("모든 패키지가 성공적으로 설치되었습니다.")
+
 
 if __name__ == "__main__":
     main()
